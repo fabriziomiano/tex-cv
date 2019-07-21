18c6f5c5b2fc0bbf3305eeef45804940f65e402f
Fabrizio Miano <fabriziomiano@gmail.com>
2019-07-21 12:37:10 +0200

    add latexmk

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,1 @@
-sudo apt install texlive texlive-luatex texlive-extra-utils texlive-fonts-recommended texlive-fonts-extra texlive-bibtex-extra biber
+sudo apt install texlive texlive-luatex texlive-extra-utils texlive-fonts-recommended texlive-fonts-extra texlive-bibtex-extra biber latexmk
